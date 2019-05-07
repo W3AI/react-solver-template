211ef5a4728be0baa55716f50fc925e8c852640a
stefian <v_ianta@yahoo.com>
2019-05-07 08:01:59 -0400

    Done items1 and 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
 
@@ -17,8 +17,8 @@ class App extends Component {
             <div className="App">
 
                 <ol>
-                    <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-                    <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+                    <li>Done! - Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+                    <li>Done! - Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
                     <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
                     <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
                     <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
@@ -31,6 +31,7 @@ class App extends Component {
                     onChange={this.inputChangeHandler} 
                     value={this.state.userInput}/>
                 <p>{this.state.userInput}</p>
+                <Validation imputLength={this.state.userInput.length} />
             </div>
         );
     }
